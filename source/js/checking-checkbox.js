c3c384f8bf569c4ccabe12deda94ff2295ac4044
Sergey Pashin <S.E.Pashin@yandex.ru>
2021-04-30 05:10:57 +0300

    Вносит правки так же проверены и исправлены ошибки линтера

--- source/js/checking-checkbox.js
+++ source/js/checking-checkbox.js
@@ -4,16 +4,20 @@ $(document).ready(function () {
   $('#form__checkbox-id').on('click', function () {
     if ( $(this).is(':checked') ) {
       $('.form__button-submit').attr('disabled', false);
+      $('.form__field input, .form textarea').attr('disabled', false);
     } else {
       $('.form__button-submit').attr('disabled', true);
+      $('.form__field input, .form textarea').attr('disabled', true);
     }
-  })
+  });
 
   $('#popup__checkbox-id').on('click', function () {
     if ( $(this).is(':checked') ) {
       $('.popup__button-submit').attr('disabled', false);
+      $('.popup__field input, .form textarea').attr('disabled', false);
     } else {
       $('.popup__button-submit').attr('disabled', true);
+      $('.popup__field input, .popup__field textarea').attr('disabled', true);
     }
-  })
+  });
 });
--- source/js/footer-menu.js
+++ source/js/footer-menu.js
@@ -3,22 +3,19 @@
 $(document).ready(function () {
 
   $('.main-nav').click(function (e) {
-    const target = e.target;
+    let target = e.target;
 
     if ($(target).is(':not(p, li, a)')
     ) {
 
       if ($('.main-nav__button').hasClass('main-nav__button--open')) {
-        $('.main-nav__button').removeClass('main-nav__button--open').addClass('main-nav__button--close');
-        $('.main-nav__first-list').removeClass('main-nav__first-list--open').addClass('main-nav__first-list--close');
-        $('.main-nav__second-list').removeClass('main-nav__second-list--open').addClass('main-nav__second-list--close');
+        closeMainNav();
 
       } else if ($('.main-nav__button').hasClass('main-nav__button--close')) {
-        $('.main-nav__button').removeClass('main-nav__button--close').addClass('main-nav__button--open');
-        $('.main-nav__first-list').removeClass('main-nav__first-list--close').addClass('main-nav__first-list--open');
-        $('.main-nav__second-list').removeClass('main-nav__second-list--close').addClass('main-nav__second-list--open');
+        openMainNav();
+        closeContacts();
       }
-  }
+    }
 
   });
 
@@ -28,12 +25,35 @@ $(document).ready(function () {
     if ($(target).is(':not(p, ul, li, a, .contacts__adress)')
     ) {
       if ($('.contacts__button').hasClass('contacts__button--open')) {
-        $('.contacts__button').removeClass('contacts__button--open').addClass('contacts__button--close');
-        $('.contacts__body').removeClass('contacts__body--open').addClass('contacts__body--close');
+        closeContacts();
       } else if ($('.contacts__button').hasClass('contacts__button--close')) {
-        $('.contacts__button').removeClass('contacts__button--close').addClass('contacts__button--open');
-        $('.contacts__body').removeClass('contacts__body--close').addClass('contacts__body--open');
+        openContacts();
+        closeMainNav();
       }
     }
   });
+
+  const openMainNav = function () {
+    $('.main-nav__button').removeClass('main-nav__button--close').addClass('main-nav__button--open');
+    $('.main-nav__first-list').removeClass('main-nav__first-list--close').addClass('main-nav__first-list--open');
+    $('.main-nav__second-list').removeClass('main-nav__second-list--close').addClass('main-nav__second-list--open');
+  }
+
+  const closeMainNav = function () {
+    $('.main-nav__button').removeClass('main-nav__button--open').addClass('main-nav__button--close');
+    $('.main-nav__first-list').removeClass('main-nav__first-list--open').addClass('main-nav__first-list--close');
+    $('.main-nav__second-list').removeClass('main-nav__second-list--open').addClass('main-nav__second-list--close');
+  }
+
+  const openContacts = function () {
+    $('.contacts__button').removeClass('contacts__button--close').addClass('contacts__button--open');
+    $('.contacts__body').removeClass('contacts__body--close').addClass('contacts__body--open');
+  }
+
+  const closeContacts = function () {
+    $('.contacts__button').removeClass('contacts__button--open').addClass('contacts__button--close');
+    $('.contacts__body').removeClass('contacts__body--open').addClass('contacts__body--close');
+  }
+
+  closeMainNav();
 });
